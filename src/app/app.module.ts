f6f3fca37c96632ba3fe4a648613eb27f0f1ff1f
ozturkmmurat <murat.oztrkk01@gmail.com>
2022-06-21 17:30:04 +0300

    19 Gün 2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,8 +27,9 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
     ToastrModule.forRoot({
-      positionClass:"toast-top-right"
+      positionClass:"toast-bottom-right"
     })
   ],
   providers: [],
